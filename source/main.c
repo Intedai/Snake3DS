bca5862878d738d0e25d2a61e6ec2f9e22595f33
Intedai <69306633+Intedai@users.noreply.github.com>
2025-06-01 03:48:33 +0300

    Added snake with movement, and an unfinished fruit that makes  the snake grow

--- source/main.c
+++ source/main.c
@@ -1,22 +1,23 @@
-
 #include <3ds.h>
 #include <citro2d.h>
-#include <stdio.h>
-
-#define SCREEN_WIDTH  400
-#define SCREEN_HEIGHT 240
+#include <string.h>
+#include <stdbool.h>
+#include "globals.h"
+#include "point.h"
+#include "snake.h"
 
 #define SQUARE_SIZE 20
 
-#define DRAW_SQUARE(x,y,z,size,color) C2D_DrawRectangle(x,y,z,size,size,color,color,color,color)
+#define DRAW_SQUARE(x,y,z,size,color) C2D_DrawRectSolid(x,y,z,size,size,color)
+#define shadow_color C2D_Color32(148, 189, 70, 255)
 
 void draw_background() {
     u32 light_color = C2D_Color32(170, 215, 81, 255);
     u32 dark_color = C2D_Color32(162, 209, 73, 255);
 
-	for (int i = 1; i < SCREEN_HEIGHT / SQUARE_SIZE - 1; i++)
+	for (int i = 1; i < TOP_SCREEN_HEIGHT / SQUARE_SIZE - 1; i++)
 	{
-		for(int j = 1; j < SCREEN_WIDTH / SQUARE_SIZE - 1; j++)
+		for(int j = 1; j < TOP_SCREEN_WIDTH / SQUARE_SIZE - 1; j++)
 		{
 			u32 curr = j % 2 == i % 2 ? light_color : dark_color;
 			DRAW_SQUARE(j * SQUARE_SIZE,   i * SQUARE_SIZE, 0, SQUARE_SIZE, curr);
@@ -24,8 +25,56 @@ void draw_background() {
 	}
 }
 
+void draw_bottom()
+{
+	u32 bottom_color = C2D_Color32(74, 117, 44, 255);
+	const int dist = 30;
+
+	C2D_DrawRectSolid(dist,dist, 0, BOTTOM_SCREEN_WIDTH - dist * 2, BOTTOM_SCREEN_HEIGHT - dist * 2, bottom_color);
+}
+
+void exit_all()
+{
+	cfguExit();
+	C2D_Fini();
+	C3D_Fini();
+	gfxExit();
+}
+
+void draw_fruit(Point position)
+{
+	int dist = SQUARE_SIZE / 5;
+	u32 fruit_color = C2D_Color32(231, 71, 29, 255);
+	DRAW_SQUARE(position.x * SQUARE_SIZE + dist, position.y * SQUARE_SIZE + dist, 0.1, SQUARE_SIZE - dist * 2, fruit_color);
+	C2D_DrawRectSolid(position.x * SQUARE_SIZE + dist, SQUARE_SIZE*position.y +  SQUARE_SIZE - dist, 0.1,SQUARE_SIZE - dist * 2, SQUARE_SIZE / 7, shadow_color);
+}
+
+void draw_snake_node(Node* node)
+{
+	Point position = node->position;
+	DRAW_SQUARE(SQUARE_SIZE*position.x,SQUARE_SIZE*position.y,1,SQUARE_SIZE,C2D_Color32(78,124,246,122));
+	
+	if (position.y != TOP_SCREEN_HEIGHT / SQUARE_SIZE - 2)
+		C2D_DrawRectSolid(SQUARE_SIZE*position.x, SQUARE_SIZE*position.y + SQUARE_SIZE,0.5,SQUARE_SIZE, SQUARE_SIZE / 5,shadow_color);
+}
+
+void update_direction(Snake* snake, u32 mask)
+{
+	if ((mask & KEY_RIGHT || mask & KEY_CSTICK_RIGHT) && snake->direction != LEFT)
+		snake->direction = RIGHT;
+	else if ((mask & KEY_LEFT || mask & KEY_CSTICK_LEFT) && snake->direction != RIGHT)
+		snake->direction = LEFT;
+	else if ((mask & KEY_DOWN || mask & KEY_CSTICK_DOWN) && snake->direction != UP)
+		snake->direction = DOWN;
+	else if ((mask & KEY_UP || mask & KEY_CSTICK_UP) && snake->direction != DOWN)
+		snake->direction = UP;
+}
+
 int main(int argc, char **argv)
 {
+	C2D_Font font = C2D_FontLoadSystem(CFG_REGION_USA);
+	u64 last_move_time = osGetTime();
+	const u64 moveDelay = 100;
 
 	// Initialize services
 	gfxInitDefault();
@@ -33,37 +82,57 @@ int main(int argc, char **argv)
 	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
 
 	C2D_Prepare();
+	
 
 	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 	C3D_RenderTarget* bottom = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
+	
 
 	u32 border_color = C2D_Color32(87, 138, 52, 255);
 
+	Point start_position = {8,8};
+
+	Snake *snake = create_snake();
+	init_snake(snake,start_position);
+
+	Point fruit = {3,3};
 	while (aptMainLoop())
 	{
 		hidScanInput();
 		u32 kDown = hidKeysDown();
 
-		if (kDown & KEY_START) break;
+		if (kDown & KEY_START) {exit_all(); break;}
+		update_direction(snake,kDown);
+
+		u64 now = osGetTime();
+		if (now - last_move_time >= moveDelay)
+		{
+			if (equal_points(fruit, snake->head->position))
+				move_snake(snake, true);
+			else
+				move_snake(snake, false);
+			last_move_time = now;
+		}
 
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-
+		if(end_game(snake)) break;
+		
 		// Draw top
-		C2D_TargetClear(top, border_color);
 		C2D_SceneBegin(top);
+		C2D_TargetClear(top, border_color);
 		draw_background();
 
 
-		// Draw bottom
-		C2D_TargetClear(bottom, border_color);
-		C2D_SceneBegin(bottom);
+		foreach_node(snake, draw_snake_node);
+		draw_fruit(fruit);
+		consoleClear();
 
+
+		C2D_SceneBegin(bottom);
+		C2D_TargetClear(bottom, border_color);
+		draw_bottom();
 		C3D_FrameEnd(0);
 	}
-
-	C2D_Fini();
-	C3D_Fini();
-	gfxExit();
-
+	exit_all();
 	return 0;
 }
---
+++ source/snake.c
@@ -0,0 +1,192 @@
+#include "snake.h"
+
+Node* new_node(Point position)
+{
+    Node *node = (Node *) malloc(sizeof(Node));
+    
+    if(!node)
+        return NULL;
+
+    node->position = position; 
+    node->next = NULL;
+    node->prev = NULL;
+    
+    return node;
+}
+
+Snake *create_snake()
+{
+    Snake *snake = (Snake *)malloc(sizeof(Snake));
+
+    // Will return NULL if allocation fails
+    return snake;
+}
+
+bool insert_at_front(Snake *snake, Point position)
+{
+    Node *node = new_node(position);
+
+    if (!node)
+        return false;
+
+    if (snake->tail == NULL)
+    {
+        snake->head = node;
+        snake->tail = node;
+    }
+    else
+    {
+        node->next = snake->head;
+        snake->head->prev = node;
+        snake->head = node; 
+    }
+
+    snake->size++;
+
+    return true;
+}
+
+/* // --------- NOT NEEDED ----------------------
+bool push_back_node(Snake *snake, Point position)
+{
+    Node *node = new_node(position);
+
+    if (!node)
+        return false;
+
+    if (snake->tail == NULL)
+    {
+        snake->head = node;
+        snake->tail = node;
+    }
+    else
+    {
+        snake->tail->next = node;
+        node->prev = snake->tail;
+        snake->tail =node;
+    }
+
+    return true;
+}
+*/
+
+bool init_snake(Snake* snake, Point starting_position)
+{
+    if (
+        starting_position.x  < START_SIZE ||
+        starting_position.x >= RIGHT_BORDER ||
+        starting_position.y <= TOP_BORDER ||
+        starting_position.y >= BOTTOM_BORDER
+
+    )
+        return false;
+    snake->size = 0;
+    snake->direction = RIGHT;
+    snake->head = NULL;
+    snake->tail = NULL;
+
+    for(int i = 0; i < START_SIZE; i++)
+    {
+        Point position = {starting_position.x - i, starting_position.y}; 
+        if(!insert_at_front(snake,position))
+            return false;
+    }
+    return true;
+}
+
+void remove_tail(Snake* snake)
+{
+    Node *tail = snake-> tail;
+
+    // Set tail to the previous node
+    snake->tail = tail->prev;
+    snake->tail->next = NULL;
+    snake->size--;
+    free(tail);
+}
+
+void move_snake(Snake* snake, bool ate_fruit)
+{
+    Point new_head_position;
+    Point old_head_position = snake->head->position;
+
+    switch (snake->direction)
+    {
+    case LEFT:
+        new_head_position.x = old_head_position.x - 1;
+        new_head_position.y = old_head_position.y + 0;
+        break;
+    case RIGHT:
+        new_head_position.x = old_head_position.x + 1;
+        new_head_position.y = old_head_position.y + 0;
+        break;
+    case UP:
+        new_head_position.x = old_head_position.x + 0;
+        new_head_position.y = old_head_position.y - 1;
+        break;
+    case DOWN:
+        new_head_position.x = old_head_position.x + 0;
+        new_head_position.y = old_head_position.y + 1;
+        break;     
+    default:
+        return; //impossible
+    }
+    
+    if (!insert_at_front(snake,new_head_position))
+        return;
+    
+    if(!ate_fruit)
+    {
+        remove_tail(snake);
+    }
+}
+
+bool end_game(Snake *snake)
+{
+    Point head_position = snake->head->position;
+    // Check if collides with walls
+    if (
+            head_position.x <= LEFT_BORDER ||
+            head_position.x >= RIGHT_BORDER ||
+            head_position.y <= TOP_BORDER ||
+            head_position.y >= BOTTOM_BORDER
+       )
+       return true;
+       
+    Node *current = snake->head->next;
+    
+    while(current != NULL)
+    {
+        if (equal_points(head_position, current->position))
+            /*return true*/; // FOR NOW THIS DOESNT WORK! IDK WHY IT RETURNS TRUE ON START
+            
+        current = current->next;
+    }
+
+    return false;
+}
+
+void foreach_node(Snake *snake, void (*func)(Node *))
+{
+    Node *current = snake-> head;
+    while (current != NULL)
+    {
+        func(current);
+        current = current -> next;
+    }
+}
+
+void free_snake(Snake *snake)
+{
+    Node *current = snake-> head;
+    Node *next;
+
+    while(current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    free(snake);
+}
