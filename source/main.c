93658df1a82b212ef838cbd81e6d3849700901ff
Intedai <69306633+Intedai@users.noreply.github.com>
2026-04-14 21:46:59 +0300

    Added a start screen using a bad state machine I will refactor

--- source/main.c
+++ source/main.c
@@ -7,11 +7,18 @@
 #include "fruit.h"
 #include "game_graphics.h"
 
+typedef enum {GAME, MENU} State;
+
 void quit_game() {
 	cfguExit();
 	C2D_Fini();
 	C3D_Fini();
 	gfxExit();
+}
+
+void free_game_resources(Snake* snake, Fruit* fruits) {
+	free_snake(snake);
+	free(fruits);
 }
 
 bool update_direction(Snake* snake, u32 mask) {
@@ -30,9 +37,142 @@ bool update_direction(Snake* snake, u32 mask) {
 	return change_direction(snake, direction);
 }
 
+void draw_menu(
+		C3D_RenderTarget* top,
+		C3D_RenderTarget* bottom
+) {
+	u32 bg_color = C2D_Color32(60, 60, 60, 255);
+	
+	// Draw top
+	C2D_SceneBegin(top);
+	C2D_TargetClear(top, bg_color);
+	
+	// Draw bottom
+	C2D_SceneBegin(bottom);
+	C2D_TargetClear(bottom, bg_color);
+}
+
+State read_menu_input(u32 kDown) {
+	if (kDown & KEY_START)
+		return GAME;
+	return MENU;
+}
+
+void init_game(
+	Snake** snake,
+	Fruit** fruits,
+	Point starting_position,
+	int starting_size,
+	u32 snake_color,
+	int row_squares,
+	int col_squares,
+	int fruit_count,
+	u32 fruit_color,
+	int fruit_value,
+	bool* chose_direction
+) {
+	*snake = create_snake();
+
+	if (!(*snake)) {
+		quit_game();
+		exit(1);
+	}
+
+	bool init_success = init_snake(
+		*snake,
+		starting_position,
+		starting_size,
+		snake_color,
+		row_squares,
+		col_squares
+	);
+
+	if (!init_success) {
+		free_snake(*snake);
+		quit_game();
+		exit(1);	
+	}
+
+	*fruits = create_fruits(fruit_count, fruit_color, fruit_value);
+
+	if(!fruits) {
+		free_snake(*snake);
+		quit_game();
+		exit(1);
+	}
+
+	init_fruits(*fruits, fruit_count, *snake, row_squares, col_squares);
+	
+	*chose_direction = false;
+}
+
+State process_game_frame(
+		bool* choseDirection,
+		Snake* snake,
+		u32 kDown,
+		int move_delay,
+		u64* last_move_time,
+		Fruit *fruits,
+		int fruit_count,
+		int row_squares,
+		int col_squares
+) {
+	if (!(*choseDirection) && update_direction(snake, kDown)) {
+		*choseDirection = true;
+	}
+
+	u64 now = osGetTime();
+	if (now - *last_move_time >= move_delay) {
+		move_snake(snake, fruit_eaten(snake, fruits, fruit_count, row_squares, col_squares));
+		*last_move_time = now;
+		*choseDirection = false;
+	}
+
+	if(is_snake_dead(snake, row_squares, col_squares)) {
+		return MENU;
+	}
+	else {
+		return GAME;
+	}
+}
+
+void draw_game_graphics(
+		C3D_RenderTarget* top,
+		C3D_RenderTarget* bottom,
+		u32 border_color,
+		int square_size,
+		int row_squares,
+		int col_squares,
+		u32 light_color,
+		u32 dark_color,
+		Snake* snake,
+		Fruit *fruits,
+		int fruit_count,
+		int fruit_margin,
+		int border_size,
+		u32 bottom_color
+) {
+	// Draw top
+	C2D_SceneBegin(top);
+	C2D_TargetClear(top, border_color);
+	draw_background(square_size, row_squares, col_squares, light_color, dark_color);
+		
+	draw_snake(snake, square_size);
+
+	for(int i = 0; i < fruit_count; i++) {
+		draw_fruit(fruits[i], square_size, fruit_margin);
+	}
+
+	// Draw bottom
+	C2D_SceneBegin(bottom);
+	C2D_TargetClear(bottom, border_color);
+	draw_bottom(border_size, bottom_color);
+} 
+
+
 int main(int argc, char **argv) {
 	u64 last_move_time = osGetTime();
-	const u64 moveDelay = 100;
+	const u64 move_delay = 100;
 
 	// Initialize services
 	gfxInitDefault();
@@ -58,6 +198,7 @@ int main(int argc, char **argv) {
 	u32 bottom_color = C2D_Color32(74, 117, 44, 255);
 
 	int fruit_margin = square_size / 5;
+	int fruit_count = 5;
 	int fruit_value = 10;
 
 	const int border_size = 30;
@@ -65,88 +206,83 @@ int main(int argc, char **argv) {
 	Point starting_position = {4, 4};
 	int starting_size = 3;
 
-	Snake *snake = create_snake();
+	State current_state = MENU;
+	bool chose_direction = false;
 
-	if (!snake) {
-		quit_game();
-		return 1;
-	}
-
-	bool init_success = init_snake(
-		snake,
-		starting_position,
-		starting_size,
-		snake_color,
-		row_squares,
-		col_squares
-	);
-
-	if (!init_success) {
-		quit_game();
-		free_snake(snake);
-		return 1;	
-	}
-
-	int fruit_count = 5;
-	Fruit *fruits = create_fruits(fruit_count, fruit_color, fruit_value);
-
-	if(!fruits) {
-		quit_game();
-		free_snake(snake);
-		return 1;
-	}
-
-	init_fruits(fruits, fruit_count, snake, row_squares, col_squares);
-	
-	bool choseDirection = false;
+	Snake* snake = NULL;
+	Fruit* fruits = NULL;
 
 	while (aptMainLoop()) {
 		hidScanInput();
 		u32 kDown = hidKeysDown();
 
-		if (kDown & KEY_START) break;
-
-		if (!choseDirection && update_direction(snake, kDown)) {
-			choseDirection = true;
-		}
-
-		u64 now = osGetTime();
-		if (now - last_move_time >= moveDelay) {
-			move_snake(snake, fruit_eaten(snake, fruits, fruit_count, row_squares, col_squares));
-			last_move_time = now;
-			choseDirection = false;
-		}
-
-		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-
-		if(is_snake_dead(snake, row_squares, col_squares)) {
-			border_color = C2D_Color32(255, 0, 0, 255);
-		};
+		switch (current_state)
+		{
+		case MENU:
+			current_state = read_menu_input(kDown);
+			if (current_state == GAME) {
+				init_game(
+					&snake,
+					&fruits,
+					starting_position,
+					starting_size,
+					snake_color,
+					row_squares,
+					col_squares,
+					fruit_count,
+					fruit_color,
+					fruit_value,
+					&chose_direction
+				);
+				break;
+			}
+			C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+			draw_menu(top, bottom);
+			break;
 		
-		// Draw top
-		C2D_SceneBegin(top);
-		C2D_TargetClear(top, border_color);
-		draw_background(square_size, row_squares, col_squares, light_color, dark_color);
-		
-		draw_snake(snake, square_size);
+		case GAME:
+			current_state = process_game_frame(
+				&chose_direction,
+				snake,
+				kDown,
+				move_delay,
+				&last_move_time,
+				fruits, fruit_count,
+				row_squares,
+				col_squares
+			);
 
-		for(int i = 0; i < fruit_count; i++) {
-			draw_fruit(fruits[i], square_size, fruit_margin);
+			if (current_state == MENU) {
+				free_game_resources(snake, fruits);
+				break;
+			}
+		
+			C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+			draw_game_graphics(
+				top,
+				bottom,
+				border_color,
+				square_size,
+				row_squares,
+				col_squares,
+				light_color,
+				dark_color,
+				snake,
+				fruits,
+				fruit_count,
+				fruit_margin,
+				border_size,
+				bottom_color
+			);
+			break;
 		}
 
-		consoleClear();
-
-		// Draw bottom
-		C2D_SceneBegin(bottom);
-		C2D_TargetClear(bottom, border_color);
-		draw_bottom(border_size, bottom_color);
 
 		C3D_FrameEnd(0);
 	}
 
+	free_game_resources(snake, fruits);
 	quit_game();
-	free_snake(snake);
-	free(fruits);
 
 	return 0;
 }
